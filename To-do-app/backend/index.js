20aa2d5f3b5e840742ac6a465ed69d9471995714
chitwang12 <chitwangandhi12@gmail.com>
2024-01-07 14:51:13 +0530

    Validations for Post and Put request done

--- To-do-app/backend/index.js
+++ To-do-app/backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {createPayload ,updatePayload} = require("./types"); 
 const app = express();
 
 app.use(express.json());
@@ -9,14 +10,30 @@ app.use(express.json());
 // }
 
 app.use("/todo",function(req,res){
+    const createPayload = req.body;
+    const parsedPayLoad = createTodo.safeParse(createPayload);
+    if(!parsedPayLoad.success){
+        res.status(411).json({
+            msg:"You sent the wrong inputs",
+        })
+        return;
+        }
+        //put it in mongo
+    })
 
-})
-
+    
 app.use("/todos",function(req,res){
 
 })
 
 
 app.use("/completed",function(req,res){
-
+const updatePayload = req.body;
+const parsedPayLoad = updateTodo.safeParse(updatePayload);
+if(!parsedPayLoad.sucess){
+    res.status(411).json({
+        msg:"You sent the wrong inputs",
+    })
+    return;
+}
 })
